e78e4825ec28e4e560a3840deb1b1036534bd2b6
claudionorcoelho <clcoelho39@gmail.com>
2016-06-04 21:15:07 -0300

    Mudança das notificações para tela de usuário
    Traduzido as notificações para melhor entendimento do usuário.

--- Cad_atividade/app/controllers/atividades_controller.rb
+++ Cad_atividade/app/controllers/atividades_controller.rb
@@ -30,7 +30,7 @@ class AtividadesController < ApplicationController
 
     respond_to do |format|
       if @atividade.save
-        format.html { redirect_to @atividade, notice: 'Atividade was successfully created.' }
+        format.html { redirect_to @atividade, notice: 'Atividade Cadastrada. successfully!' }
         format.json { render :show, status: :created, location: @atividade }
       else
         format.html { render :new }
@@ -44,7 +44,7 @@ class AtividadesController < ApplicationController
   def update
     respond_to do |format|
       if @atividade.update(atividade_params)
-        format.html { redirect_to @atividade, notice: 'Atividade was successfully updated.' }
+        format.html { redirect_to @atividade, notice: 'Atividade Alterada. successfully!' }
         format.json { render :show, status: :ok, location: @atividade }
       else
         format.html { render :edit }
@@ -58,7 +58,7 @@ class AtividadesController < ApplicationController
   def destroy
     @atividade.destroy
     respond_to do |format|
-      format.html { redirect_to atividades_url, notice: 'Atividade was successfully destroyed.' }
+      format.html { redirect_to atividades_url, notice: 'Atividade Deletada. successfully!' }
       format.json { head :no_content }
     end
   end
@@ -71,6 +71,6 @@ class AtividadesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def atividade_params
-      params.require(:atividade).permit(:descricao, :palestrante, :local)
+      params.require(:atividade).permit(:descricao, :palestrante, :local, :data_inicio, :data_fim)
     end
 end
--- Cad_atividade/app/views/atividades/show.json.jbuilder
+++ Cad_atividade/app/views/atividades/show.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract! @atividade, :id, :descricao, :palestrante, :local, :created_at, :updated_at
+json.extract! @atividade, :id, :descricao, :palestrante, :local, :data_inicio, :data_fim, :created_at, :updated_at
